21c1e4af12422ece21e2b553c6afe186249247ff
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-10-14 16:37:47 +0530

    Add files via upload

---
+++ 1422_A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int t; cin >> t;
+    while(t--)
+    {
+        int a[3];
+        for(int i=0; i<3; i++)
+            cin >> a[i];
+        sort(a, a+3);
+        cout << a[2] + 1 << "\n";
+    }
+}
